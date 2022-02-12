4db5dcd546f751ce1e8ebabbea47ba709af0dce6
schleising <stephen@schleising.net>
2022-02-12 12:28:40 +0000

    Fix past dates and add the year if not in this week

--- suntimes.py
+++ suntimes.py
@@ -29,17 +29,27 @@ if __name__=='__main__':
             print('Could not parse date, showing sunset time for today')
         sunsetDate = datetime.today()
 
+    # Assume the date is in the future
+    timeInPast = False
+
     # Find the difference between the requested date and today
     match (sunsetDate.date() - date.today()).days:
         # Today and the requested date are the same
         case 0:
             dayString = 'Today'
+
         # The requested date is one day into the future (i.e., tomorrow)
         case 1:
             dayString = 'Tomorrow'
+
         # The requested date is more than a week away, in this case print the day of month and month
-        case dayDiff if dayDiff > 6:
-            dayString = sunsetDate.strftime('On %A %d %B')
+        case dayDiff if dayDiff > 6 or dayDiff < 0:
+            dayString = sunsetDate.strftime('On %A %d %B %Y')
+
+            # Register that the date is in the past
+            if dayDiff < 0:
+                timeInPast = True
+
         # The requested date is in this week, but not today or tomorrow, so just print the day name
         case _:
             dayString = sunsetDate.strftime('On %A')
@@ -49,5 +59,5 @@ if __name__=='__main__':
 
     # Print the sunset time with the formatted day string
     print()
-    print(f'{dayString} the sun will set at {sunset.hour:02}:{sunset.minute:02}')
+    print(f'{dayString} the sun {"" if timeInPast else "will "}set at {sunset.hour:02}:{sunset.minute:02}')
     print()
